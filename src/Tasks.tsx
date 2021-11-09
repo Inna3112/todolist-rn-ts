f22c110a9e54ec6809d694b460d9377a912bb010
Inna3112 <innula3112@gmail.com>
2021-11-09 15:50:21 +0200

    task styles updated

--- src/Tasks.tsx
+++ src/Tasks.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 type PropsType = {
   tasks: TaskType[];
 };
 export const Tasks: React.FC<PropsType> = ({tasks}) => {
   return (
-    <View>
+    <View style={styles.container}>
       {tasks.map(task => (
-        <Text>{task.title}</Text>
+        <View style={styles.task}>
+          <Text>{task.title}</Text>
+        </View>
       ))}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 5,
+  },
+  task: {
+    backgroundColor: '#eee',
+    marginBottom: 2,
+    padding: 4,
+  },
+});
 
 export type TaskType = {
   id: string;
